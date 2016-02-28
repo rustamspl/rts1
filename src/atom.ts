1dfa4c0cc4f38050a03cdddc04e2e2346ea2c664
R <r@local>
2016-02-28 11:09:04 +0600

    1

--- src/atom.ts
+++ src/atom.ts
@@ -1,5 +1,31 @@
+
+class MyClass2 {
+    @log2
+    myMethod(arg: string) { 
+        return "Message -- " + arg;
+    }
+    getCb(){
+        return (a)=>{return a**3};
+    }
+}
+
+function log2(target: Object, propertyKey: string, descriptor: TypedPropertyDescriptor<any>) {
+    var originalMethod = descriptor.value; // save a reference to the original method
+
+    // NOTE: Do not use arrow syntax here. Use a function expression in 
+    // order to use the correct value of `this` in this method (see notes below)
+    descriptor.value = function(...args: any[]) {
+        console.log("The method args are: " + JSON.stringify(args)); // pre
+        var result = originalMethod.apply(this, args);               // run and store the result
+        console.log("The return value is: " + result);               // post
+        return result;                                               // return the result of the original method
+    };
+
+    return descriptor;
+}
+
 export = class Atom {
-    r:any;
+    r=new MyClass2();
     constructor() {
 
     }
